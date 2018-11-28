37af4209d9ce94e8ba06ffcddd398a79a6f23f18
PiyaliS <40656182+PiyaliS@users.noreply.github.com>
2018-11-28 13:30:20 +0530

    blah

--- Javascript/js/js1.js
+++ Javascript/js/js1.js
@@ -1,10 +1,15 @@
+function date(){
+ 	var d= new Date();
+ 	document.getElementById("time").innerHTML = d + "<br>" + "Click to update time.";
+	}
 function CloseMe(){
 	x=document.getElementById("demo");
-	x.style.display="none";
+	x.style.visibility="hidden";
 }
 function OpenMe()
 {
 	x=document.getElementById("demo");
+	x.style.visibility="visible";
 	x.style.display="block";
 }
 function displayId(element){
@@ -13,3 +18,8 @@ function displayId(element){
 function SP(element){
 	document.getElementById("message").innerHTML= element.alt;
 }
+
+function SP2(element){
+	document.getElementById("message").innerHTML = 'Hover over an image';
+}
+ 
